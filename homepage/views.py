47eca6a14e0c1274f6926f6b62ecd106a1045c01
Mater <yjeafar@gmail.com>
2019-10-27 19:11:57 -0400

    Backup some files

--- homepage/views.py
+++ homepage/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from login.models import FarmOwner
+from django.http import Http404
 
 
 # Create your views here.
-def index(results):
+def index(request):
     farm_owners = FarmOwner.name #change this once data is added to the database
-    template = loader.get_template('homepage/home.html') 
-    context = {
-        'farm_owner': farm_owners
-    }
-    return HttpResponse(template.render(context, results))
+    context = { 'farm_owner': farm_owners}
+    return render(request, 'homepage/homepage.html', context)
 
+# def details(request, farm_user):
+#     try: 
+#         farm_owner = FarmOwner.username.get(pk=farm_user)
+#     except FarmOwner.DoesNotExist:
+#         raise Http404("User does not exist")
+#     return render(request, 'homepage/homepage.html', { 'farm_owner': farm_owner})
 
